03f23d76ceb3a87660dcd07514ab0092fe64b371
BossHectorius <121318695+BossHectorius@users.noreply.github.com>
2026-03-25 23:44:33 +0100

    c4

--- square/script/chunk.gd
+++ square/script/chunk.gd
@@ -4,10 +4,15 @@ class_name Chunk
 @export var size: Vector2 = Vector2(16, 16)
 @export var res: Vector2 = Vector2(16, 16)
 
+var first_row: Array
+var right_neigh: Chunk
+var down_neigh: Chunk
+var diag_neigh: Chunk
 var points: Array[Array]= []
 
 
-func _generate_chunk(noise: FastNoiseLite, noise_offset: Vector2 = Vector2(0, 0)) -> void:
+
+func _generate_chunk(noise: FastNoiseLite, noise_offset: Vector2 = Vector2(0, 0),) -> void:
 	for x in size.x:
 		points.append([])
 		for y in size.y:
@@ -15,61 +20,94 @@ func _generate_chunk(noise: FastNoiseLite, noise_offset: Vector2 = Vector2(0, 0)
 			value = smoothstep(0, 0, value)
 			points[x].append([])
 			points[x][y] = Vector4(x * res.x, y * res.y, value, 0)
+	first_row.append(points.front())
 	draw.emit()
 
-func _generate_shape(plist: Array) -> void:
-	for x in range(0, plist.size() -1):
-		for y in range(0, plist.size() - 1):
-			var a: Vector4 = plist[x][y]
-			var b: Vector4 = plist[x + 1][y]
-			var c: Vector4 = plist[x + 1][y + 1]
-			var d: Vector4 = plist[x][y + 1]
-			var ahat := Vector2(a.x, a.y)
-			var bhat := Vector2(b.x, b.y)
-			var chat := Vector2(c.x, c.y)
-			var dhat := Vector2(d.x, d.y)
-			var total := _to_base10(a.z, b.z, c.z, d.z)
-			var A: Vector2 = Vector2(a.x + res.x * 0.5, a.y)
-			var B: Vector2 = Vector2(b.x, b.y + res.y * 0.5)
-			var C: Vector2 = Vector2(c.x - res.x * 0.5, c.y)
-			var D: Vector2 = Vector2(d.x, d.y - res.y * 0.5)
-			match total:
-				0:
-					pass
-				1:
-					_create_polygon([C, dhat, D])
-				2:
-					_create_polygon([C, chat, B])
-				3:
-					_create_polygon([D, dhat, chat, B])
-				4:
-					_create_polygon([A, bhat, B])
-				5:
-					_create_polygon([bhat, B, C, dhat, D, A])
-				6:
-					_create_polygon([A, bhat, chat, C])
-				7:
-					_create_polygon([A, bhat, chat, dhat, D])
-				8:
-					_create_polygon([ahat, A, D])
-				9:
-					_create_polygon([ahat, A, C, dhat])
-				10:
-					_create_polygon([ahat, A, B, chat, C, A])
-				11:
-					_create_polygon([ahat, A, B, chat, dhat])
-				12:
-					_create_polygon([ahat, bhat, B, D])
-				13:
-					_create_polygon([ahat, bhat, B, C, dhat])
-				14:
-					_create_polygon([ahat, bhat, chat, C, D])
-				15:
-					_create_polygon([ahat, bhat, chat, dhat])
+class Cell:
+	var value := 0
+	var edges: Dictionary = {
+		"a" : null,
+		"b" : null,
+		"c" : null,
+		"d" : null
+	}
+	
+	func _init(a: Vector4, b: Vector4, c: Vector4, d: Vector4) -> void:
+		self.edges["a"] = a
+		self.edges["b"] = b
+		self.edges["c"] = c
+		self.edges["d"] = d
+
+func _generate_shape() -> void:
+	if right_neigh:
+		points.append_array(right_neigh.first_row)
+	if down_neigh:
+		for X in range(0, down_neigh.points.size()):
+			points[X].append(down_neigh.points[X][down_neigh.points[X].size() - 1])
+	if diag_neigh:
+		points[17].append(diag_neigh.points.front().front())
+	for x in range(0, points.size()):
+		for y in range(0, points[x].size()):
+			var a: Vector4 = points[x][y]
+			var b: Vector4 = points[x + 1][y]
+			var c: Vector4 = points[x + 1][y + 1]
+			var d: Vector4 = points[x][y + 1]
+			triangulate(Cell.new(a, b, c, d))
+
 
 
 func _to_base10(a: int, b: int, c: int, d: int) -> int:
 	return a * 8 + b * 4 + c * 2 + d * 1
+
+func triangulate(cell: Cell) -> void:
+	var a: Vector4 = cell.edges.get("a")
+	var b: Vector4 = cell.edges.get("b")
+	var c: Vector4 = cell.edges.get("c")
+	var d: Vector4 = cell.edges.get("d")
+	var ahat := Vector2(a.x, a.y)
+	var bhat := Vector2(b.x, b.y)
+	var chat := Vector2(c.x, c.y)
+	var dhat := Vector2(d.x, d.y)
+	@warning_ignore("narrowing_conversion")
+	var total := _to_base10(a.z, b.z, c.z, d.z)
+	var A: Vector2 = Vector2(a.x + res.x * 0.5, a.y)
+	var B: Vector2 = Vector2(b.x, b.y + res.y * 0.5)
+	var C: Vector2 = Vector2(c.x - res.x * 0.5, c.y)
+	var D: Vector2 = Vector2(d.x, d.y - res.y * 0.5)
+	cell.value = total
+	match total:
+		0:
+			pass
+		1:
+			_create_polygon([C, dhat, D])
+		2:
+			_create_polygon([C, chat, B])
+		3:
+			_create_polygon([D, dhat, chat, B])
+		4:
+			_create_polygon([A, bhat, B])
+		5:
+			_create_polygon([bhat, B, C, dhat, D, A])
+		6:
+			_create_polygon([A, bhat, chat, C])
+		7:
+			_create_polygon([A, bhat, chat, dhat, D])
+		8:
+			_create_polygon([ahat, A, D])
+		9:
+			_create_polygon([ahat, A, C, dhat])
+		10:
+			_create_polygon([ahat, A, B, chat, C, A])
+		11:
+			_create_polygon([ahat, A, B, chat, dhat])
+		12:
+			_create_polygon([ahat, bhat, B, D])
+		13:
+			_create_polygon([ahat, bhat, B, C, dhat])
+		14:
+			_create_polygon([ahat, bhat, chat, C, D])
+		15:
+			_create_polygon([ahat, bhat, chat, dhat])
 
 func _create_polygon(p: Array) -> void:
 	var polygon := Polygon2D.new()
--- square/script/world.gd
+++ square/script/world.gd
@@ -1,9 +1,9 @@
 extends Node2D
 class_name WorldManager
 
-var chunks: Array[Array]
+var chunks: Dictionary
 @export_category("World size")
-@export var wsize: Vector2 = Vector2(16, 16)
+@export var wsize: Vector2 = Vector2(8, 8)
 @export var chunk_size: Vector2
 
 
@@ -19,34 +19,31 @@ func _generate_start(noise: FastNoiseLite) -> void:
 	var xoff: int = 0
 	var yoff: int = 0
 	for x in range(0, wsize.x):
-		xoff += 1
-		chunks.append([])
 		for y in range(0, wsize.y):
-			chunks[x].append([])
-			yoff += 1
 			var chunk := Chunk.new()
 			chunk._generate_chunk(noise, noise_offset)
 			noise_offset += chunk.size
 			chunk.position = Vector2(chunk_size.x * xoff, chunk_size.y * yoff)
 			add_child(chunk)
-			chunks[x][y] = chunk
-			chunk._generate_shape(chunk.points)
-			
+			chunks[Vector2(xoff, yoff)] = chunk
+			yoff += 1
 		yoff = 0
+		xoff += 1
 	xoff = 0
-	#for c in chunks:
-		#for chunk in c:
-			#if chunk is Chunk:
-				#var cright = chunks[xoff + 1][yoff] as Chunk
-				#var cdown = chunks[xoff][yoff + 1] as Chunk
-				#var cdig = chunks[xoff + 1][yoff + 1] as Chunk
-				#var crightp: Array = cright.points[0]
-				#chunk.points.append(crightp)
-				#for x in cdown.points:
-					#if x is Array:
-						#print(" length of row %s is %s and its value is %s" % [xoff, len(x), x[len(x) - 1]])
-						#chunk.points[xoff].append(x[len(x) - 1])
-						#xoff += 1
-				#var clen := len(cdig.points)
-				#chunk.points[8].append(cdig.points[clen - 1][clen - 1])
-				#xoff = 0
+	for c in range(0, wsize.x):
+		for y in range(0, wsize.y):
+			var cright: Chunk = null
+			var cdown: Chunk = null
+			var cdig: Chunk = null
+			if chunks.has(Vector2(c+1, y)):
+				cright = chunks[Vector2(c + 1, y)] as Chunk
+			if chunks.has(Vector2(c, y + 1)):
+				cdown = chunks[Vector2(c, y + 1)] as Chunk
+			if chunks.has(Vector2(c + 1, y + 1)):
+				cdig = chunks[Vector2(c+1, y+1)]
+			var chunk = chunks[Vector2(c, y)] as Chunk
+			chunk.down_neigh = cdown
+			chunk.right_neigh = cright
+			chunk.diag_neigh = cdig
+			chunk._generate_shape()
+		xoff += 1
